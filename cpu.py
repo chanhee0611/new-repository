a02638ade86bc7644bad105a53103ebc9fb310b9
chanhee0611 <sn2023159@shs.hs.kr>
2024-11-27 18:20:34 +0900

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -21,18 +21,6 @@ plt.xticks(rotation=45)
 plt.ylim(1400,1700)
 st.pyplot(fig1)
 
-amd_cpu5_m = amd_cpu.sort_values('multiScore', ascending=False).head(5)
-
-fig2=plt.figure(figsize=(10, 6))
-plt.bar(amd_cpu5_m['cpuName'], amd_cpu5_m['multiScore'])
-for idx, txt in enumerate(amd_cpu5_m['multiScore']):
-    plt.text(idx, txt+3, str(txt), ha='center',color='blue')
-plt.xlabel('CPU Name')
-plt.ylabel('multi Score')
-plt.title('AMD CPU mutli score top 5')
-plt.xticks(rotation=45)
-plt.ylim(40000,80000)
-st.pyplot(fig2)
 
 intel_cpu = intel_cpu.sort_values('singleScore', ascending=False).head(7)
 
@@ -47,41 +35,3 @@ plt.xticks(rotation=45)
 plt.ylim(1900,2100)
 st.pyplot(fig3)
 
-intel_cpu5 = intel_cpu.sort_values('multiScore', ascending=False).head(5)
-
-fig4=plt.figure(figsize=(10, 6))
-plt.bar(intel_cpu5['cpuName'], intel_cpu5['multiScore'])
-for idx, txt in enumerate(intel_cpu5['multiScore']):
-    plt.text(idx, txt+3, str(txt), ha='center',color='blue')
-plt.xlabel('CPU Name')
-plt.ylabel('multi Score')
-plt.title('Intel CPU multi score top 5')
-plt.xticks(rotation=45)
-plt.ylim(25000,28000)
-st.pyplot(fig4)
-
-cpu7_s = cpu.sort_values('singleScore', ascending=False).head(7)
-
-fig5=plt.figure(figsize=(10, 6))
-plt.bar(cpu7_s['cpuName'], cpu7_s['singleScore'])
-for idx, txt in enumerate(cpu7_s['singleScore']):
-    plt.text(idx, txt+3, str(txt), ha='center',color='blue')
-plt.xlabel('CPU Name')
-plt.ylabel('Single Score')
-plt.title('all CPU single score top 7')
-plt.xticks(rotation=45)
-plt.ylim(1900,2100)
-st.pyplot(fig5)
-
-cpu5_m = cpu.sort_values('multiScore', ascending=False).head(5)
-
-fig6=plt.figure(figsize=(10, 6))
-plt.bar(cpu5_m['cpuName'], cpu5_m['multiScore'])
-for idx, txt in enumerate(cpu5_m['multiScore']):
-    plt.text(idx, txt+3, str(txt), ha='center',color='blue')
-plt.xlabel('CPU Name')
-plt.ylabel('multi Score')
-plt.title('all CPU multi top 5')
-plt.xticks(rotation=45)
-plt.ylim(40000,80000)
-st.pyplot(fig6)
